3447eb33935fc5f0d0960d913f97b3150f41354e
serbs <1123@gmail.com>
2025-05-02 14:47:04 +0300

    field

--- src/app/auth/Auth.tsx
+++ src/app/auth/Auth.tsx
@@ -1,28 +1,45 @@
-import { authService } from '@/services/auth.service';
-import { IAuthForm } from '@/types/auth.types';
-import { useMutation } from '@tanstack/react-query';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
+"use client";
 
-'use client'
+import { DASHBOARD_PAGES } from "@/config/pages-url.config";
+import { authService } from "@/services/auth.service";
+import { IAuthForm } from "@/types/auth.types";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "sonner";
 
 export const Auth = () => {
-    const { register, handleSubmit, reset } = useForm<IAuthForm>({
-        mode: 'onChange'
-    })
+  const { register, handleSubmit, reset } = useForm<IAuthForm>({
+    mode: "onChange",
+  });
 
-    const [isLoginForm, setIsLoginForm] = useState(false);
+  const [isLoginForm, setIsLoginForm] = useState(false);
 
-    const { push } = useRouter()
+  const { push } = useRouter();
 
-    const { } = useMutation({
-        mutationKey: ['auth'],
-        mutationFn: (data: IAuthForm) => authService.main(isLoginForm ? 'login' : 'register', data),
-        onSuccess
-    })
+  const { mutate } = useMutation({
+    mutationKey: ["auth"],
+    mutationFn: (data: IAuthForm) =>
+      authService.main(isLoginForm ? "login" : "register", data),
+    onSuccess() {
+      toast.success("Successfully login!");
+      reset();
+      push(DASHBOARD_PAGES.HOME);
+    },
+  });
 
-    return (
-        <div></div>
-    );
+  const onSubmit: SubmitHandler<IAuthForm> = (data) => {
+    mutate(data);
+  };
+  return (
+    <div className="flex min-h-screen">
+      <form
+        className="w-1/4 m-auto shadow bg-sidebar rounded-xl p-layout"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="flex items-center gap-5 justify-center"></div>
+      </form>
+    </div>
+  );
 };
