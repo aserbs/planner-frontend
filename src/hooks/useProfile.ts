9f42a8f79ba0fd3e4aeeeec1d743c6335cf4f12a
lelele <aleshkaserb2003@gmail.com>
2025-05-16 13:40:12 +0300

    settings page

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -2,11 +2,11 @@ import { userService } from "@/services/user.service";
 import { useQuery } from "@tanstack/react-query";
 
 export function useProfile() {
-    const { data, isLoading } = useQuery({
+    const { data, isLoading, isSuccess } = useQuery({
         queryKey: ['profile'],
         queryFn: () => userService.getProfile()
     })
 
 
-    return { data, isLoading }
+    return { data, isLoading, isSuccess }
 }
